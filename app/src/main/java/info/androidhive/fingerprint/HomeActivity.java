f9b8c712fd1abba210393e96e39573aaadc11596
Sourabh Jigjinni <sourabhjigjinni@gmail.com>
2018-06-02 09:42:53 +0530

    web view

--- app/src/main/java/info/androidhive/fingerprint/HomeActivity.java
+++ app/src/main/java/info/androidhive/fingerprint/HomeActivity.java
@@ -1,11 +1,17 @@
 package info.androidhive.fingerprint;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
 
 public class HomeActivity extends AppCompatActivity {
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,6 +19,38 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        WebView webView = findViewById(R.id.webView);
+
+        webView.getSettings().setSupportZoom(false);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setHorizontalScrollBarEnabled(false);
+
+        webView.loadUrl("http://google.com");
+
     }
 
+    @Override
+    public void onBackPressed() {
+        //DO NOTHING TO PREVENT BACK PRESS
+    }
+
+    // Ensure the right menu is setup
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    // Start your settings activity when a menu item is selected
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.logout) {
+            Intent i = new Intent(getApplicationContext(), FingerprintActivity.class);
+            startActivity(i);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
